9bc145ef1570cf5b708c16f1f67cc13c2f3aac94
Andre Hirsch Oliveira <hirsch.oliveira@unifei.edu.br>
2021-12-13 15:31:22 -0300

    Added first part of character generation, so we have data to add to the tree.

--- main.c
+++ main.c
@@ -2,11 +2,29 @@
 #include <stdlib.h>
 #include <string.h>
 #include "tree_Search.h"
+#include <time.h>
+
+Data character_generation(char *name, int size);
 
 
-
-int main(int argc, char const *argv[])
+int main()
 {
+    char name[15] = "bob"; 
+    Data bob = character_generation(name, 15); 
     
+    printf("%s | %d | %d | %d",bob.name,bob.mind,bob.body,bob.level); 
+
     return 0;
+}
+
+Data character_generation(char *name, int size){
+    srand(time(NULL)); 
+
+    Data new_character; 
+    strcpy(new_character.name, name);
+    new_character.body = rand()%17 + 1;
+    new_character.mind = rand()%17 + 1;
+    new_character.level = rand()%99 + 1; 
+
+    return new_character; 
 }
